894345c9b4be7d2df9b9a367c4d5c2cce5ad8448
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-07-05 13:04:54 +0530

    UI impovement needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [keyword, setKeyword] = useState('');
+  const [shayari, setShayari] = useState('');
+
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleGenerateClick = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/generate?type=Shayari', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ keyword }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setShayari(data.join('\n'));
+      } else {
+        console.log('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shayari Generator</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter a keyword"
+          value={keyword}
+          onChange={handleKeywordChange}
+        />
+        <button onClick={handleGenerateClick}>Generate Shayari</button>
+      </div>
+      {shayari && (
+        <div className="shayari-container">
+          <h2>Generated Shayari:</h2>
+          <p>{shayari}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,35 +1,76 @@
-const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
+import React, { useState } from 'react';
+import {
+  Box,
+  Heading,
+  Input,
+  Button,
+  Text,
+  Container,
+  VStack,
+  Center,
+} from '@chakra-ui/react';
+import './App.css';
 
-const app = express();
-const port = process.env.PORT || 5000;
+const App = () => {
+  const [keyword, setKeyword] = useState('');
+  const [shayari, setShayari] = useState('');
 
-app.use(express.json());
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
 
-app.get('/shayari', async (req, res) => {
-  try {
-    const keyword = req.query.keyword;
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-      prompt: `Shayari about ${keyword}`,
-      max_tokens: 100,
-      temperature: 0.7,
-      n: 1
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
+  const handleGenerateClick = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/generate?type=Shayari', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ keyword }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setShayari(data.join('\n'));
+      } else {
+        console.log('Error:', response.statusText);
       }
-    });
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  };
 
-    const shayari = response.data.choices[0].text.trim();
-    res.json({ shayari });
-  } catch (error) {
-    console.error('Error:', error.response.data);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
+  return (
+    <Center minH="100vh" bg="gray.100">
+      <Container maxW="sm" p={4} bg="white" rounded="md" shadow="md">
+        <VStack spacing={4} align="stretch">
+          <Heading as="h1" size="2xl" textAlign="center">
+            Shayari Generator
+          </Heading>
+          <Input
+            placeholder="Enter a keyword"
+            value={keyword}
+            onChange={handleKeywordChange}
+          />
+          <Button
+            colorScheme="teal"
+            onClick={handleGenerateClick}
+            isDisabled={!keyword}
+          >
+            Generate Shayari
+          </Button>
+          {shayari && (
+            <Box borderWidth="1px" borderRadius="md" p={4}>
+              <Text fontWeight="bold" mb={2}>
+                Generated Shayari:
+              </Text>
+              <Text>{shayari}</Text>
+            </Box>
+          )}
+        </VStack>
+      </Container>
+    </Center>
+  );
+};
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default App;
